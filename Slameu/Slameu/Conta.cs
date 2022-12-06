72b439f9d4b15227d169e3cc0e57f0d97d4f234b
Raiden-Nervoso <116876648+Raiden-Nervoso@users.noreply.github.com>
2022-12-06 11:57:20 +0000

    Add files via upload

---
+++ Slameu/Slameu/Conta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slameu
+{
+    internal class Conta
+    {
+        
+        public Conta(int numero, double limite)
+        {
+            this.Numero = numero;
+            this.Limite = limite;
+        }
+        private double Saldo { get; set; }
+        public double Limite { get; private set; }
+        public int Numero { get; private set; }
+
+        public void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+        public bool Saca(double valor)
+        {
+            double SaldoDisponivel = this.ConsultaSaldoDisponivel();
+            
+
+            if(valor > SaldoDisponivel)
+            {
+                Console.WriteLine("Saque e saldo indisponivel");
+                return false;
+            }
+            this.Saldo -= valor;
+            return true;
+        }
+        public void AdicionarLimite(double valor)
+        {
+            this.Limite = valor;
+        }
+
+        public double ConsultaSaldoDisponivel()
+        {
+            return this.Saldo + this.Limite;
+        }
+    }
+}
---
+++ Slameu/Slameu/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slameu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo POO");
+
+            //criando instancia conta
+            Conta conta = new Conta(123, 300);
+
+            //a conta começa com 300 euros e estes 300 não são usados
+
+            conta.Deposita(1500);
+            conta.Deposita(2500);
+            conta.Deposita(4500);
+
+            
+
+            bool saca = conta.Saca(7000);
+
+            if(saca)
+            {
+                double saldo = conta.ConsultaSaldoDisponivel();
+
+                Console.WriteLine("O seu saldo é: " + saldo);
+                Console.WriteLine("O seu saldo limite é: " + conta.Limite);
+                Console.WriteLine("O numero da sua conta é: " + conta.Numero);
+            }
+        }
+    }
+}
