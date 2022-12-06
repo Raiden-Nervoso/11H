16d1fbb21df30d9868f3a0fdcf5e4cc250880eb5
Raiden-Nervoso <116876648+Raiden-Nervoso@users.noreply.github.com>
2022-12-06 12:10:12 +0000

    Add files via upload

---
+++ Mod09 cena da aula qualquer/Mod09 cena da aula qualquer/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod09_cena_da_aula_qualquer
+{
+    public class Class1
+    {
+       /* public Class1(int numero)
+        {
+            this.Numero = numero;
+        }
+        public Class1(int Numero , double Limite)
+        {
+            this.Numero = Numero;
+            this.Limite = Limite;
+        }*/
+        private double Saldo { get; set; }
+
+        public double Limite { get; private set; }
+        public double Numero { get; set; }
+        
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+        public bool Tirar(double valor)
+        {
+            double SaldoDisponivel = this.ConsultaSaldo();
+           if (valor > SaldoDisponivel )
+           {
+                Console.WriteLine("Saldo negativo");
+                return false;
+           }
+            this.Saldo -= valor;
+            return true;
+        }
+        public void AddLimite(double valor)
+        {
+            this.Limite = valor;
+        }
+        public double ConsultaSaldo()
+        {
+            return this.Saldo + this.Limite;    
+        }
+    }
+}
---
+++ Mod09 cena da aula qualquer/Mod09 cena da aula qualquer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod09_cena_da_aula_qualquer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Class1 conta3 = new Class1();
+
+            conta3.AddLimite(1500);
+
+            conta3.Depositar(1500);
+            conta3.Depositar(2500);
+            conta3.Depositar(4500);
+
+            int decisao = 0;
+            Console.WriteLine("O seu saldo neste momento é de: " + conta3.ConsultaSaldo());
+            Console.WriteLine("O limite é de: " + conta3.Limite);
+            Console.WriteLine("Pretende retirar quanto dinheiro? ");
+            decisao = int.Parse(Console.ReadLine());
+
+            bool tira = conta3.Tirar(decisao);
+
+            if(tira)
+            {
+            double saldo = conta3.ConsultaSaldo();
+                Console.WriteLine("O seu saldo é: " + saldo);
+                Console.WriteLine("O seu limite é: " + conta3.Limite);
+                
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
